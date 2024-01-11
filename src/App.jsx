66fd8b72ef1dbad39a1b8fb94d07bb0e451de620
SumonsNote <sohankhance@gmail.com>
2024-01-12 00:04:39 +0600

    complete assignment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import StudentsTable from "./components/StudentsTable";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Section />
+      <StudentsTable />
+      <Footer />
+    </div>
+  );
+}
